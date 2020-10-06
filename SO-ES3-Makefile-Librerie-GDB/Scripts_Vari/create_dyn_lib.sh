dec6d5276a73b08cdb5621b580082d069f157174
Luigi De Simone <luigi.desimone@unina.it>
2020-10-06 12:43:02 +0200

    rm

--- SO-ES3-Makefile-Librerie-GDB/Scripts_Vari/create_dyn_lib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Compilare modulo sorgente
-gcc -c -fpic mat.c
-#Caso 1 compilazione libreria statica
-gcc -shared -Wl,-soname,libmat.so -o libmat.so mat.o
-
--- SO-ES3-Makefile-Librerie-GDB/Scripts_Vari/create_stat_lib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Compilare modulo sorgente
-gcc -c mat.c
-
-#Compilazione libreria statica
-ar rcs libmat.a mat.o
-
