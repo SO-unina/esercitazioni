75a1e78da668265d2d7d5127d60eac49aadf8f8b
Luigi De Simone <luigi.desimone@unina.it>
2020-11-12 19:03:08 +0100

    fix names

--- SO-ES6-Semafori/2_semaphores_1ex/semafori.c
+++ SO-ES6-Semafori/2_semaphores_1ex/semafori.c
@@ -1,106 +1,41 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/sem.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <errno.h>
 
-#include "procedure.h"
+#include "semafori.h"
 
 
+int Wait_Sem (int id_sem, int numsem) {
+       int err;
+       struct sembuf sem_buf;
 
-int main() {
+       sem_buf.sem_num=numsem;
+       sem_buf.sem_flg=0;
+       sem_buf.sem_op=-1;
 
-	key_t k_shmem = IPC_PRIVATE;
-	key_t k_sem = IPC_PRIVATE;
+       err = semop(id_sem,&sem_buf,1);   //semaforo rosso
 
-	int id_shmem = shmget(k_shmem, sizeof(int), IPC_CREAT|0664);
+       if(err<0) {
+         perror("Errore WAIT");
+       }
 
-	if(id_shmem < 0) {
-		perror("Errore SHMGET");
-		exit(1);
-	}
-
-
-	int id_sem = semget(k_sem, 1, IPC_CREAT|0664); // QUANTI SEMAFORI HO?
-
-	if(id_sem < 0) {
-		perror("Errore SEMGET");
-		exit(1);
-	}
+       return err;
+}
 
 
-	int * ptr = shmat(id_shmem, 0, 0);
+int Signal_Sem (int id_sem, int numsem) {
+       int err;
+       struct sembuf sem_buf;
 
-	if(ptr == (void *)-1) {
-		perror("Errore SHMAT");
-		exit(1);
-	}
+       sem_buf.sem_num=numsem;
+       sem_buf.sem_flg=0;
+       sem_buf.sem_op=1;
 
+       err = semop(id_sem,&sem_buf,1);   //semaforo verde
 
-	semctl(id_sem,0,SETVAL,1);
-	printf("SEMAFORO %d INIZIALIZZATO AD 1\n", id_sem);
+       if(err<0) {
+         perror("Errore SIGNAL");
+       }
 
-
-	*ptr = 0;
-	printf("VALORE INIZIALE: %d\n", *ptr);
-
-
-	int i;
-	for(i=0; i<2; i++) {	// CREA 2 PROCESSI FIGLI
-
-		pid_t pid = fork();
-
-		if(pid < 0) {
-			perror("Errore FORK");
-			exit(1);
-		}
-
-		if(pid==0) {
-			// PROCESSO FIGLIO
-			printf("Process[%d] PID=%d creato\n", i, getpid());
-			int j;
-			for(j=0; j<100; j++) {
-
-				Wait_Sem(id_sem, 0);
-
-				//INIZIO SEZIONE CRITICA
-
-				int tmp = *ptr;
-                                printf("Processo %d ha letto\n", i);
-				
-                                // CORRETTO: *ptr = tmp + 1;
-                                // SBAGLIATO: *ptr++;
-                                // SBAGLIATO: *(ptr)++;
-                                // CORRETTO: (*ptr)++;
-
-                                *ptr = tmp + 1;
-
-                                printf("Processo %d ha incrementato\n", i);
-
-				//FINE SEZIONE CRITICA
-
-				Signal_Sem(id_sem, 0);
-
-			}
-
-			exit(0);
-		}
-	}
-
-
-	for(i=0; i<2; i++) {
-		wait(NULL);
-	}
-
-
-	printf("VALORE FINALE: %d\n", *ptr);//Quanto vale il valore finale?
-
-	shmctl(id_shmem,IPC_RMID,0);
-	semctl(id_sem,0,IPC_RMID);
-
-	return 0;
+       return err;
 }
 
